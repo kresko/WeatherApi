aac28a0763f689f1956fa21c30a3611e25a06ced
kresko <kresko1995@gmail.com>
2023-11-16 22:49:21 +0100

    Revert "Testing"
    This reverts commit 66ed3c48a0493fddbfc68065cef55b5911ba5a00.

--- js/app.js
+++ js/app.js
@@ -241,6 +241,3 @@ function toggleTemperatureUnit(searchInputValue, glossaryKey) {
 function toggleTemperatureUnitButtonContent(buttonElement, currentTempUnit = "°C") {
     buttonElement.innerHTML = "Display: " + "<span class='current-temp-unit-element'>" + currentTempUnit + "</span>"; 
 }
-
-//test comment
-const comment = "dfgdf";
